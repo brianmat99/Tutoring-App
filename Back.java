81f1920218dea230e2320aa47d76f283ae1a239d
Kelvin Nguyen <kelvin.nguyen.360@gmail.com>
2020-12-10 05:36:11 -0800

    Added days of week when getting tutors from DB

--- Back.java
+++ Back.java
@@ -42,21 +42,142 @@ public class Back {
 
     // TODO: Create functions for each use case. Will probably be using PreparedStatements a lot
 
-    public int getNumOfTutorsByCourse(String co) {
+    public int getNumOfTutorsByCourse(String co, String[] d) {
         try {
-            String sql = "select count(*) " +
-                "from Tutor, CoursesSupported " +
-                "where tutor_id = cs_tutor_id " +
-                    "and course_name = ? ";
-            PreparedStatement stmt = c.prepareStatement(sql);
-            stmt.setString(1, co);
+            if(d.length == 1) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and day_of_week = ? ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
 
-            ResultSet rs = stmt.executeQuery();
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
+            if(d.length == 2) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
 
-            int num = rs.getInt(1);
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
+            if(d.length == 3) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
+            if(d.length == 4) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
+            if(d.length == 5) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+                stmt.setString(6, d[4]);
+
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
+            if(d.length == 6) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+                stmt.setString(6, d[4]);
+                stmt.setString(7, d[5]);
+
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
+            if(d.length == 7) {
+                String sql = "select count(distinct tutor_id) " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+                stmt.setString(6, d[4]);
+                stmt.setString(7, d[5]);
+                stmt.setString(8, d[6]);
+
+                ResultSet rs = stmt.executeQuery();
+                int num = rs.getInt(1);
+                
+                return num;
+            }
             
-            return num;
-
         } catch(Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
@@ -64,29 +185,174 @@ public class Back {
         return -1;  // Error
     }
 
-    public String[] getTutorsByCourse(String co) {
-        String[] failure = new String[1234];
+    public String[] getTutorsByCourse(String co, String[] d) {
+        String[] failure = new String[123];
         try {
-            int numOfTutors = getNumOfTutorsByCourse(co);
-            String[] tutors = new String[numOfTutors];
+            int num = getNumOfTutorsByCourse(co, d);
+            String[] tutors = new String[num];
 
-            String sql = "select Tutor.name " +
-                "from Tutor, CoursesSupported " +
-                "where tutor_id = cs_tutor_id " +
-                    "and course_name = ? ";
-            PreparedStatement stmt = c.prepareStatement(sql);
-            stmt.setString(1, co);
+            if(d.length == 1) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and day_of_week = ? ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
 
-            ResultSet rs = stmt.executeQuery();
-
-            int i = 0;
-            while(rs.next()) {
-                tutors[i] = rs.getString(1);
-                i++;
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
             }
+            if(d.length == 2) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
 
-            return tutors;
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
+            }
+            if(d.length == 3) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
 
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
+            }
+            if(d.length == 4) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
+            }
+            if(d.length == 5) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+                stmt.setString(6, d[4]);
+
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
+            }
+            if(d.length == 6) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+                stmt.setString(6, d[4]);
+                stmt.setString(7, d[5]);
+
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
+            }
+            if(d.length == 7) {
+                String sql = "select distinct Tutor.name " +
+                    "from Tutor, CoursesSupported, Availability " +
+                    "where tutor_id = cs_tutor_id " +
+                        "and tutor_id = a_tutor_id " +
+                        "and course_name = ? " +
+                        "and (day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ? or day_of_week = ?) ";
+                PreparedStatement stmt = c.prepareStatement(sql);
+                stmt.setString(1, co);
+                stmt.setString(2, d[0]);
+                stmt.setString(3, d[1]);
+                stmt.setString(4, d[2]);
+                stmt.setString(5, d[3]);
+                stmt.setString(6, d[4]);
+                stmt.setString(7, d[5]);
+                stmt.setString(8, d[6]);
+
+                ResultSet rs = stmt.executeQuery();
+                int i = 0;
+                while(rs.next()) {
+                    tutors[i] = rs.getString(1);
+                    i++;
+                }
+                
+                return tutors;
+            }
+            
         } catch(Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
@@ -139,7 +405,7 @@ public class Back {
 
         return failure; // Should not reach here...
     }
-    
+
     public int getCoursesCount() {
         try {
             
@@ -186,5 +452,55 @@ public class Back {
 
         return failure; // If return this then function failed
     }
-    
+
+    /*
+    public int getNumOfTutorReviews(int t) {
+        try {
+            String sql = "select count(*) " +
+                "from Reviews " +
+                "where r_tutor_id = ? ";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setInt(1, t);
+
+            ResultSet rs = stmt.executeQuery();
+
+            int num = rs.getInt(1);
+
+            return num;
+
+        } catch(Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+        return -1;  // Error
+    }
+
+    public String[] getTutorReviews(int t) {
+        String[] failure = new String[1234];
+        try {
+            int numOfReviews = getNumOfTutorReviews(t);
+            String[] reviews = new String[numOfReviews];
+            String sql = "select comment " +
+                "from Reviews " +
+                "where r_tutor_id = ? ";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setInt(1, t);
+
+            ResultSet rs = stmt.executeQuery();
+
+            int i = 0;
+            while(rs.next()) {
+                
+                i++;
+            }
+
+            return ;
+
+        } catch(Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+        return failure;  // Error
+    }
+    */
 }
